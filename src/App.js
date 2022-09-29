d7ae7f60e30a12361dd13fc86fdc6f54d8115d03
Tony Tang <tonytang213@gmail.com>
2022-09-29 14:11:05 -0500

    navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './Assets/Styles/App.css';
 import NavigationBar from './Components/NavigationBar';
+import {Box} from '@mui/material';
 
 export default function App() {
   return (
-    <div className="Body">
-      <NavigationBar/>
-    </div>
+		<div className='Body'>
+			<Box sx={{ flexGrow: 1 }}>
+				<NavigationBar />
+			</Box>
+		</div>
   );
 }
--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -8,29 +8,30 @@ import MenuIcon from '@mui/icons-material/Menu';
 
 function NavigationBar() {
 	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position='static'>
-				<Toolbar>
-					<IconButton
-						size='large'
-						edge='start'
-						color='inherit'
-						aria-label='menu'
-						sx={{ mr: 2 }}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography
-						variant='h6'
-						component='div'
-						sx={{ flexGrow: 1 }}
-					>
-						News
-					</Typography>
-					<Button color='inherit'>Login</Button>
-				</Toolbar>
-			</AppBar>
-		</Box>
+		<AppBar
+			position='static'
+			sx={{ bgcolor: 'orange' }}
+		>
+			<Toolbar>
+				<IconButton
+					size='large'
+					edge='start'
+					color='inherit'
+					aria-label='menu'
+					sx={{ mr: 2 }}
+				>
+					<MenuIcon />
+				</IconButton>
+				<Typography
+					variant='h6'
+					component='div'
+					sx={{ flexGrow: 1 }}
+				>
+					Nib-Nav
+				</Typography>
+				<Button color='inherit'>Login</Button>
+			</Toolbar>
+		</AppBar>
 	);
 }
 
