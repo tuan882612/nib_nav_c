ad948109728102c49aaed49a1c2af833d3582cdf
Dhrutik-S <solankidhrutik0602@gmail.com>
2022-11-30 16:55:04 -0600

    Didn't change profile

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,112 +1,48 @@
 import {
-	Button, TextField,Dialog,DialogActions,DialogContent,DialogContentText,DialogTitle
-} from '@mui/material';
-import '../Assets/Styles/Profile.css';
-import { useState } from 'react';
-import axios from 'axios';
-import React from 'react';
+  Button,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import "../Assets/Styles/Profile.css";
+import { useState } from "react";
+import axios from "axios";
+import React from "react";
 
-function Profile() {	
-	const[open,setOpen] = React.useState(false);
+function Profile() {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-	const handleClickOpen = () => {
-		setOpen(true);
-	}
+  if (sessionStorage.getItem("login") === "true") {
+    axios
+      .get("http://localhost:8080/user/get/" + sessionStorage.getItem("id"))
+      .then((response) => {
+        const body = response.data;
+        setValues({
+          ...values,
+          name: body.name,
+          email: body.email,
+          password: body.password,
+        });
+      });
+  }
 
-	const handleClickClose = () => {
-		setOpen(false);
-	}
+  return (
+    <div className="main-container">
+      <div className="wrapper-container">
+        <div id="profile-header">Profile</div>
+        <div className="profile-contents">Name: {values.name}</div>
 
-	const [values, setValues] = useState({
-		name:'',
-		email:'',
-		password:''
-	})
-
-    if (sessionStorage.getItem('login') === 'true') {
-        axios.get('http://localhost:8080/user/get/' + sessionStorage.getItem('id'))
-        .then((response) => {
-			const body = response.data
-			setValues({...values, 
-				name: body.name,
-				email: body.email,
-				password: body.password
-			})
-		});
-    }
-
-	return (
-		<div className='main-container'>
-			<div className='wrapper-container'>
-				<div id='profile-header'>Profile</div>
-				<div className='profile-contents'>Name: <Button
-					sx={{
-						backgroundColor: 'inherit',
-						color: 'white',
-						'&:hover': { backgroundColor: '#495A57' },
-						display: 'inline',
-					}}
-					color='inherit'
-					variant='outlined'
-					onClick={handleClickOpen}
-					
-				>
-					{values.name}
-				</Button>
-				<Dialog open ={open} onClose={handleClickClose}>
-					<DialogTitle>Edit Credentials</DialogTitle>
-					<DialogContent>
-						<DialogContentText>
-							Change Name
-						</DialogContentText>
-						<TextField 
-						autoFocus
-						margin = "dense"
-						id = "Name"
-						label = {values.name}
-						type = "Name"
-						fullWidth
-						variant= "standard"
-						InputProps={{style:{fontSize:30}}}
-						InputLabelProps={{style : {fontSize:30}}}/>
-					</DialogContent>
-				</Dialog></div>
-				<div className='profile-contents'>Email: <Button
-					sx={{
-						backgroundColor: 'inherit',
-						color: 'white',
-						'&:hover': { backgroundColor: '#495A57' },
-						display: 'inline',
-					}}
-					color='inherit'
-					variant='outlined'
-					onClick={handleClickOpen}
-					
-				>
-					{values.email}
-				</Button>
-				<Dialog open ={open} onClose={handleClickClose}>
-					<DialogTitle>Edit Credentials</DialogTitle>
-					<DialogContent>
-						<DialogContentText>
-							Change Email
-						</DialogContentText>
-						<TextField 
-						autoFocus
-						margin = "dense"
-						id = "Email"
-						label = {values.email}
-						type = "email"
-						fullWidth
-						variant= "standard"
-						InputProps={{style:{fontSize:30}}}
-						InputLabelProps={{style : {fontSize:30}}}/>
-					</DialogContent>
-				</Dialog></div>
-				
-			</div>
-		</div>
-	);
+        <div className="profile-contents">Email: {values.email}</div>
+      </div>
+    </div>
+  );
 }
 
 export default Profile;
